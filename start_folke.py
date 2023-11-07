3cafc3e088a44840aaf367f7c18b3045d1201b6a
emigus27 <116457302+emigus27@users.noreply.github.com>
2023-11-07 16:44:08 +0100

    updated start

--- start_folke.py
+++ start_folke.py
@@ -11,6 +11,7 @@ from pycowview.plot import plot_barn
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 #%%
 #
@@ -35,18 +36,49 @@ def remove_stationary_tags(df):
 df_new, stationary_list = remove_stationary_tags(df)
 tags_cow = [tag for tag in tags if tag not in stationary_list]
 #%%
+def time_of_day(start_time, time_to_convert, time_zone):
+    seconds_diff = (time_to_convert-start_time)/1_000
+    time_of_day = datetime.timedelta(seconds=seconds_diff+time_zone*3_600)
+    return time_of_day
+
+time = time_of_day(1600732800023, 1600751577548, 2)
+#%%
+
+
+
+
+def divide_groups(df, tags, x_divide):
+    x_avg = df[1_000_000:].groupby('tag_id')['x'].mean()
+    tags_g1 = np.array([tag for tag in tags if x_avg[tag] >= x_divide])
+    tags_g2 = np.array([tag for tag in tags if x_avg[tag] < x_divide])
+    df_g1 = df[df['tag_id'].isin(tags_g1)] 
+    df_g2 = df[df['tag_id'].isin(tags_g2)]
+    return df_g1, df_g2, tags_g1, tags_g2
+
+
+df_g1, df_g2, tags_g1, tags_g2 = divide_groups(df_new, tags_cow, 1_670)
+
+#%%
+#def milk_times(df)
+n_splits = 12
+y_arr = np.zeros(n_splits)
+for i, interval in  enumerate(np.array_split(df_g1, n_splits)):
+    y_arr[i] = interval['y'].mean()
+indices = np.argpartition(y_arr, 2)[:2]
+
+#%%
 milking_dct = {tag:False for tag in tags_cow}
 entry_times = {tag:[] for tag in tags_cow}
 exit_times = {tag:[] for tag in tags_cow}
 #%%
-for row in df_new[:3000000].iterrows():
+for row in df_new[:4000000].iterrows():
     tag = row[1]['tag_id']
     if milking_dct[tag] == False:
         if row[1]['y'] <= 2000 and 1400 < row[1]['x'] < 2000:
             entry_times[tag].append(row[1]['time'])
             milking_dct[tag] = True
     else:
-        if row[1]['y'] >= 2000 and 1400 < row[1]['x'] < 2000 and entry_times[tag][-1]+45*60_000<row[1]['time']:
+        if row[1]['y'] >= 2000 and 1400 < row[1]['x'] < 2000 and entry_times[tag][-1]+20*60_000<row[1]['time']:
             exit_times[tag].append(row[1]['time'])
             milking_dct[tag] = False
          
@@ -55,7 +87,7 @@ for row in df_new[:3000000].iterrows():
 
 #%%
 
-plot_cow(df_new[:2500000], 2420330, barn_filename)
+plot_cow(df_new[2940252:], tag, barn_filename)
 
 #%%
 #fig, ax = plot_barn(barn_filename)
